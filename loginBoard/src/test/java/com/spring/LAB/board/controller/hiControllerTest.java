8339b6845d9167e5b7ed1521db51ab2133a557ff
Hyejung Yang <y39scode@gmail.com>
2022-04-20 20:32:19 +0900

    back to eclipse

--- loginBoard/src/test/java/com/spring/LAB/board/controller/hiControllerTest.java
+++ loginBoard/src/test/java/com/spring/LAB/board/controller/hiControllerTest.java
@@ -1,17 +1,14 @@
 package com.spring.LAB.board.controller;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -36,10 +33,11 @@ public class hiControllerTest {
 	void hiTest2() throws Exception{
 		String id = "test1";
 		String pw = "1234";
-		mvc.perform(get("/test/hi2"))
-				.param("id", id)
-				.param("pw," pw)
+		mvc.perform(get("/test/hi2")
+					.param("id", id)
+					.param("pw", pw))
 				.andExpect(status().isOk())
-				.andExpect(content().string(hi));
+				.andExpect(jsonPath("$.id", is(id)))
+				.andExpect(jsonPath("$.pw", pw).exists());
 	}
 }
